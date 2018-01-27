fabd92822ea7273cfdd5b5bbb732a21982e02944
krlargo <krlargo@ucdavis.edu>
2018-01-27 04:52:03 -0800

    Add missing cloned files

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import Home from './containers/Home';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Home />
+  </Provider>,
+  document.querySelector('.container')
+);
