716e395846acf249bec0912b9d189a71a5c6b3aa
krlargo <krlargo@ucdavis.edu>
2018-01-28 03:07:19 -0800

    Add ProfilePanel, SkillsPanel, TechnologiesPanel to index

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,4 @@
+export ProfilePanel from './ProfilePanel';
+export SkillsPanel from './SkillsPanel';
+export Technologies from './TechnologiesPanel';
 export ProjectHighlight from './ProjectHighlight';
