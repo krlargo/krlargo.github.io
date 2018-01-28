1c3a61379340495b8bf668e73bc040cc0399ef60
krlargo <krlargo@ucdavis.edu>
2018-01-27 19:15:14 -0800

    Add navigation links

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -2,7 +2,29 @@ import React, { Component } from 'react';
 
 class Header extends Component {
   render() {
-    return <div className="header">Hedader</div>;
+    return (
+      <div className="header">
+        <div className="navbar">
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">About</a>
+            </li>
+            <li>
+              <a href="/">Portfolio</a>
+            </li>
+            <li>
+              <a href="/">Resume</a>
+            </li>
+            <li>
+              <a href="/">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
 
