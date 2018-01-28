c167f193a187088deb65346243a6d9e7cf4aded4
krlargo <krlargo@ucdavis.edu>
2018-01-27 19:16:34 -0800

    Setup basic LSP layout

--- src/containers/LeftSidePanel.js
+++ src/containers/LeftSidePanel.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 
 class LeftSidePanel extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      profileImageURL: './src/images/profile-pictures/napa.jpg',
+      profileContent:
+        'Computer Science graduate from UC Davis looking to land a software engineering role in the Bay Area.'
+    };
+  }
+
   render() {
-    // Nested container divs required for floating left panel
+    const { profileImageURL, profileContent } = this.state;
     return (
-      <div className="left-side-panel">
-        <div className="left-side-panel-content">Left Side Panel</div>
+      <div>
+        <div className="left-side-panel">
+          <div className="section">
+            <img src={profileImageURL} />
+            <div className="profile-content">{profileContent}</div>
+            <button
+              className="btn btn-default"
+              style={{ margin: '10px' }}
+              onClick={() => {
+                console.log('CLICKED');
+              }}
+            >
+              GitHub
+            </button>
+            <a href="https://github.com/krlargo">Github</a>
+            <a href="https://wwww.linkedin.com/in/kevinlargo">LinkedIn</a>
+            <a href="https://itunes.apple.com/us/developer/kevin-largo/id1269350192?mt=8">
+              App Store Link
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
