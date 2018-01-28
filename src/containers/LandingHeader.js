f052fb408c24b5d83eedc484eb83fd738597c99b
krlargo <krlargo@ucdavis.edu>
2018-01-27 19:15:36 -0800

    Add landing header text

--- src/containers/LandingHeader.js
+++ src/containers/LandingHeader.js
@@ -2,7 +2,23 @@ import React, { Component } from 'react';
 
 class LandingHeader extends Component {
   render() {
-    return <div className="landing-header">Landing Header</div>;
+    return (
+      <div className="landing-header">
+        <div className="landing-header-text">
+          {'{'}
+          <br />
+          &nbsp; {"name: 'Kevin Largo'"}
+          <br />
+          &nbsp; {"education: 'UC Davis'"}
+          <br />
+          &nbsp; {"degree: 'Computer Science'"}
+          <br />
+          &nbsp; {"location: 'Bay Area'"}
+          <br />
+          {'}'}
+        </div>
+      </div>
+    );
   }
 }
 
