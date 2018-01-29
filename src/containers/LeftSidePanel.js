125c4f7a113597900125d97211c8ca7793551594
krlargo <krlargo@ucdavis.edu>
2018-01-29 03:34:17 -0800

    Fix div layering

--- src/containers/LeftSidePanel.js
+++ src/containers/LeftSidePanel.js
@@ -4,10 +4,8 @@ import { SkillsPanel, ProfilePanel } from '../components';
 class LeftSidePanel extends Component {
   render() {
     return (
-      <div>
-        <div className="left-side-panel">
-          <ProfilePanel />
-        </div>
+      <div className="left-side-panel">
+        <ProfilePanel />
       </div>
     );
   }
