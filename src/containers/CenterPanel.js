f8a6ffd25a1219367f9f1d3ac87e8c81a860796f
krlargo <krlargo@ucdavis.edu>
2018-01-28 16:39:11 -0800

    Rename Content -> CenterPanel

---
+++ src/containers/CenterPanel.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class CenterPanel extends Component {
+  render() {
+    // Nested container divs required for floating left panel
+    return <div className="center-panel">Center Panel</div>;
+  }
+}
+
+export default CenterPanel;
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,6 +1,6 @@
 export Home from './Home';
 export Header from './Header';
 export LandingHeader from './LandingHeader';
-export Content from './Content';
+export CenterPanel from './CenterPanel';
 export LeftSidePanel from './LeftSidePanel';
 export RightSidePanel from './RightSidePanel';
