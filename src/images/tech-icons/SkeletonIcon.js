2cbb56f7c833035e0bf3b43c7444c4bccf198e04
krlargo <krlargo@ucdavis.edu>
2018-01-28 22:56:47 -0800

    Setup .svg tech icon components

---
+++ src/images/tech-icons/SkeletonIcon.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+export const CppIcon = props => {
+  const { height, width, color } = props;
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 50 50"
+      height={height}
+      width={width}
+    >
+      x
+    </svg>
+  );
+};
+
+export const CppIconColored = props => {
+  const { height, width, color } = props;
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 50 50"
+      height={height}
+      width={width}
+    >
+      x
+    </svg>
+  );
+};
---
+++ src/images/tech-icons/index.js
@@ -0,0 +1,12 @@
+// Skills
+export { CppIcon } from './CppIcon';
+export { FirebaseIcon } from './FirebaseIcon';
+export { JavaIcon } from './JavaIcon';
+export { JavaScriptIcon } from './JavaScriptIcon';
+export { ReactIcon } from './ReactIcon';
+export { SwiftIcon } from './SwiftIcon';
+
+//Profile
+export { GitHubIcon } from './GitHubIcon';
+export { LinkedInIcon } from './LinkedInIcon';
+export { AppStoreIcon } from './AppStoreIcon';
