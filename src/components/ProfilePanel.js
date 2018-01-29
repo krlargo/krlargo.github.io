04b605032ddb9ee277c26f06be27ff947b4d47a3
krlargo <krlargo@ucdavis.edu>
2018-01-29 00:41:11 -0800

    Move icon sizing to css, use variable for icon coloring

--- src/components/ProfilePanel.js
+++ src/components/ProfilePanel.js
@@ -7,32 +7,33 @@ class ProfilePanel extends Component {
 
     this.state = {
       profileImageURL: './src/images/profile-pictures/napa.jpg',
-      profileContent:
-        'Computer Science graduate from UC Davis looking to land a software engineering role in the Bay Area.'
+      profileSummary:
+        "I'm a Computer Science graduate from UC Davis looking to land a software engineering role in the Bay Area."
     };
   }
 
   render() {
-    const { profileImageURL, profileContent } = this.state;
+    const { profileImageURL, profileSummary } = this.state;
+    const iconColor = '#333';
+
     return (
       <div className="profile-panel">
         <div className="section">
-          <img src={profileImageURL} />
           <div className="profile-content">
-            {profileContent}
-
+            <img src={profileImageURL} />
+            <div className="profile-summary">{profileSummary}</div>
             <div className="profile-link">
-              <AppStoreIcon height={25} width={25} />
+              <AppStoreIcon color={iconColor} />
               <a href="https://itunes.apple.com/us/developer/kevin-largo/id1269350192?mt=8">
                 App Store Link
               </a>
             </div>
             <div className="profile-link">
-              <GitHubIcon height={26} width={26} />
+              <GitHubIcon color={iconColor} />
               <a href="https://github.com/krlargo">GitHub</a>
             </div>
             <div className="profile-link">
-              <LinkedInIcon height={22} width={22} color={'black'} />
+              <LinkedInIcon color={iconColor} />
               <a href="https://www.linkedin.com/in/kevinlargo">LinkedIn</a>
             </div>
           </div>
