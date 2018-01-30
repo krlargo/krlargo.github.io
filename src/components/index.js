4d02cdbe57f837733b3d33ff0190efcec3f9b6a4
krlargo <krlargo@ucdavis.edu>
2018-01-30 07:22:14 -0800

    Import About and Experience Panels

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
+export AboutPanel from './AboutPanel';
+export ExperiencePanel from './ExperiencePanel';
 export ProfilePanel from './ProfilePanel';
 export ProjectHighlight from './ProjectHighlight';
 export SkillsPanel from './SkillsPanel';
