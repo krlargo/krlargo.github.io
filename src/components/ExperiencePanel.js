e40501a9935f437970a165d8e939bff295fd1b7f
krlargo <krlargo@ucdavis.edu>
2018-01-31 20:17:15 -0800

    Remove excess dummy data

--- src/components/ExperiencePanel.js
+++ src/components/ExperiencePanel.js
@@ -16,22 +16,9 @@ class ExperiencePanel extends Component {
       ]
     };
 
-    const experience2 = {
-      company: 'Garcon LLC',
-      position: 'iOS Developer Intern',
-      responsibilities: [
-        'Worked as the primary iOS engineer tasked with the development of the iOS platform using Swift, Objective-C and Xcode’s Interface Builder.',
-        'Designed data structures and data models to manage persisted data retrieved from Firebase.',
-        'Developed the user interface based on the design team’s specifications by implementing custom view controllers and programming custom segue transitions.',
-        'Verified that views, controls, and gestures were consistent with Apple’s Human Interface Guidelines to present users with views that conform to iOS standards.',
-        'Collaborated with the business team to assess time estimation, feasibility, and complexity of proposed application features.'
-      ]
-    };
-
     this.state = {
       experience: {
-        experience1,
-        experience2
+        experience1
       }
     };
   }
