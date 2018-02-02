be8c469a9353be6e41a1de66645ec88fdf6c5079
krlargo <krlargo@ucdavis.edu>
2018-02-01 20:48:13 -0800

    Import XIcon

--- src/images/tech-icons/index.js
+++ src/images/tech-icons/index.js
@@ -6,7 +6,10 @@ export { JavaScriptIcon } from './JavaScriptIcon';
 export { ReactIcon } from './ReactIcon';
 export { SwiftIcon } from './SwiftIcon';
 
-//Profile
+// Profile
 export { GitHubIcon } from './GitHubIcon';
 export { LinkedInIcon } from './LinkedInIcon';
 export { AppStoreIcon } from './AppStoreIcon';
+
+// X
+export { XIcon } from './XIcon';
