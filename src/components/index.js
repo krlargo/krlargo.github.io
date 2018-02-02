ffb74e1fc5633da79c5b4ec9f1524b76120338a3
krlargo <krlargo@ucdavis.edu>
2018-02-02 00:47:11 -0800

    Delete unused Components

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,3 @@
-export AboutPanel from './AboutPanel';
-export WorkHistoryPanel from './WorkHistoryPanel';
-export ExperiencePanel from './ExperiencePanel';
 export ProfilePanel from './ProfilePanel';
 export ProjectHighlight from './ProjectHighlight';
 export SkillsPanel from './SkillsPanel';
