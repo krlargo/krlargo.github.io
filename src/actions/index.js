1e35353c4fdd6312996e9a5eb2e9a6d227d65530
krlargo <krlargo@ucdavis.edu>
2018-02-01 23:46:31 -0800

    Temporarily comment out unused actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,9 @@
-import {
-  fetchProfileSummary,
-  fetchProfileImageURL
-} from 'fetch_profile_action';
-import { fetchAbout } from 'fetch_about_action';
-import { fetchTechnicalSkills } from 'fetch_technical_skills';
-import { fetchWorkHistory } from 'fetch_work_history';
-import { fetchExperience } from 'fetch_experience';
-import { fetchFeaturedProjects } from 'fetch_featured_projects';
+//export {
+//  fetchProfileSummary,
+//  fetchProfileImageURL
+//} from './fetch_profile_action';
+export { fetchAbout } from './fetch_about_action';
+//export { fetchTechnicalSkills } from './fetch_technical_skills';
+//export { fetchWorkHistory } from './fetch_work_history';
+//export { fetchExperience } from './fetch_experience';
+//export { fetchFeaturedProjects } from './fetch_featured_projects';
