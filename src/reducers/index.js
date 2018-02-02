9bb7beaba530e13511940be35a3271a43aafc204
krlargo <krlargo@ucdavis.edu>
2018-02-01 22:33:26 -0800

    Setup Reducers index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,19 @@
 import { combineReducers } from 'redux';
+import { aboutReducer } from './about_reducer';
+import { workHistoryReducer } from './work_history_reducer';
+import { experienceReducer } from './experience_reducer';
+import { profileSummaryReducer, profileImageReducer } from './profile_reducer';
+import { technicalSkillsReducer } from './technical_skills_reducer';
+import { featuredProjectsReducer } from './featured_projects_reducer';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  about: aboutRedcuer,
+  workHistory: workHistoryReducer,
+  experience: experienceReducer,
+  profileSummary: profileSummaryReducer,
+  profileImage: profileImageReducer,
+  technicalSkills: technicalSkillsReducer,
+  featuredProjects: featuredProjectsReducer
 });
 
 export default rootReducer;
