170a7ff8b1074e7e2a5e40f62e7f61ee1c410560
krlargo <krlargo@ucdavis.edu>
2018-02-02 00:33:48 -0800

    Uncomment workHistory action/reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,9 @@
+export { fetchAbout } from './fetch_about_action';
+export { fetchWorkHistory } from './fetch_work_history_action';
 //export {
 //  fetchProfileSummary,
 //  fetchProfileImageURL
 //} from './fetch_profile_action';
-export { fetchAbout } from './fetch_about_action';
-//export { fetchTechnicalSkills } from './fetch_technical_skills';
-//export { fetchWorkHistory } from './fetch_work_history';
-//export { fetchExperience } from './fetch_experience';
-//export { fetchFeaturedProjects } from './fetch_featured_projects';
+//export { fetchTechnicalSkills } from './fetch_technical_skills_action';
+//export { fetchExperience } from './fetch_experience_action';
+//export { fetchFeaturedProjects } from './fetch_featured_projects_action';
