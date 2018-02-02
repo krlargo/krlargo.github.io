17ade3ac9fdf73d9f696d283c8eca6df88493d71
krlargo <krlargo@ucdavis.edu>
2018-02-01 23:46:54 -0800

    Add Center Section to index

--- src/containers/index.js
+++ src/containers/index.js
@@ -3,5 +3,6 @@ export Header from './Header';
 export ContactModal from './ContactModal';
 export LandingHeader from './LandingHeader';
 export CenterPanel from './CenterPanel';
+export CenterSection from './CenterSection';
 export LeftSidePanel from './LeftSidePanel';
 export RightSidePanel from './RightSidePanel';
