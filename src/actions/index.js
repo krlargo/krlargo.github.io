a822ce98d42b33f6b221dd88fdc0cd20ae55d162
krlargo <krlargo@ucdavis.edu>
2018-02-01 22:33:12 -0800

    Setup Action Creators index

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+import {
+  fetchProfileSummary,
+  fetchProfileImageURL
+} from 'fetch_profile_action';
+import { fetchAbout } from 'fetch_about_action';
+import { fetchTechnicalSkills } from 'fetch_technical_skills';
+import { fetchWorkHistory } from 'fetch_work_history';
+import { fetchExperience } from 'fetch_experience';
+import { fetchFeaturedProjects } from 'fetch_featured_projects';
