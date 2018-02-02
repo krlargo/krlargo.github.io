cb15b56adf34952b531910de80914cdc466e6508
krlargo <krlargo@ucdavis.edu>
2018-02-01 23:49:07 -0800

    Setup Redux, get About working with CenterSection

--- src/containers/CenterPanel.js
+++ src/containers/CenterPanel.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'react-redux';
 import { AboutPanel, ExperiencePanel, WorkHistoryPanel } from '../components';
+import { CenterSection } from '.';
+import { fetchAbout } from '../actions';
 
 class CenterPanel extends Component {
+  componentDidMount() {
+    this.props.fetchAbout();
+  }
+
   render() {
+    const { about } = this.props;
+
     // Nested container divs required for floating left panel
     return (
       <div className="center-panel">
-        <AboutPanel />
+        <CenterSection title="About" content={about.content} />
         <WorkHistoryPanel />
         <ExperiencePanel />
       </div>
@@ -14,4 +24,8 @@ class CenterPanel extends Component {
   }
 }
 
-export default CenterPanel;
+const mapStateToProps = ({ about }) => {
+  return { about };
+};
+
+export default connect(mapStateToProps, { fetchAbout })(CenterPanel);
