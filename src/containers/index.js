9823daf49e99be29a74dfb8a0cfba9db3fbe89ee
krlargo <krlargo@ucdavis.edu>
2018-02-01 20:48:28 -0800

    Import Contact Modal

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,5 +1,6 @@
 export Home from './Home';
 export Header from './Header';
+export ContactModal from './ContactModal';
 export LandingHeader from './LandingHeader';
 export CenterPanel from './CenterPanel';
 export LeftSidePanel from './LeftSidePanel';
