cdaa84a36b514d26d9c65e7f3a7a0722eb191a98
krlargo <krlargo@ucdavis.edu>
2018-02-03 01:06:48 -0800

    Add Contact & Resume Components

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,6 @@ export CenterSection from './CenterSection';
 export LeftSidePanelComponent from './LeftSidePanelComponent';
 export RightSidePanelComponent from './RightSidePanelComponent';
 export FeaturedProject from './FeaturedProject';
+export Resume from './Resume';
+export Contact from './Contact';
 export Popup from './Popup';
