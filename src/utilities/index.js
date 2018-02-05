ba05ee12337ca4c38d69d3ff6f0a6b8ed6b038ad
krlargo <krlargo@ucdavis.edu>
2018-02-04 16:27:32 -0800

    Updates

--- src/utilities/index.js
+++
@@ -1,6 +0,0 @@
-export const getScrollDistance = () => {
-  return window.pageYOffset !== undefined
-    ? window.pageYOffset
-    : (document.documentElement || document.body.parentNode || document.body)
-        .scrollTop;
-};
