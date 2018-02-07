2fe09da702d5f9d49fe999bf51c957a347df0e77
krlargo <krlargo@ucdavis.edu>
2018-02-06 19:11:43 -0800

    Add projectURL property

--- src/actions/fetch_featured_projects_action.js
+++ src/actions/fetch_featured_projects_action.js
@@ -13,7 +13,8 @@ export const fetchFeaturedProjects = () => dispatch => {
         'Google AdMob'
       ],
       imageURL:
-        'https://docs.google.com/drawings/d/e/2PACX-1vT0RkJhLV_FlhnNj7CijrT8XWiTq9zpt65llbyubZwM3sHH6wD-87JuNdAS1llNEAUArUE0v9ZoRS1l/pub?w=1048&h=1048'
+        'https://docs.google.com/drawings/d/e/2PACX-1vT0RkJhLV_FlhnNj7CijrT8XWiTq9zpt65llbyubZwM3sHH6wD-87JuNdAS1llNEAUArUE0v9ZoRS1l/pub?w=1048&h=1048',
+      projectURL: 'https://krlargo.github.io/CodeWords'
     },
     {
       name: 'SyncedLists',
@@ -27,7 +28,8 @@ export const fetchFeaturedProjects = () => dispatch => {
         'Google AdMob'
       ],
       imageURL:
-        'https://docs.google.com/drawings/d/e/2PACX-1vT37SFAa9jyGZ1VrbNgjvemvSALQSLn4cQTqShjr5OrOJuc1H-88naiso9_J4_FqQdzq_gpB-2vxGJ8/pub?w=1049&h=1049'
+        'https://docs.google.com/drawings/d/e/2PACX-1vT37SFAa9jyGZ1VrbNgjvemvSALQSLn4cQTqShjr5OrOJuc1H-88naiso9_J4_FqQdzq_gpB-2vxGJ8/pub?w=1049&h=1049',
+      projectURL: 'https://krlargo.github.io/SyncedLists'
     },
     {
       name: 'QuickSplit',
@@ -41,7 +43,8 @@ export const fetchFeaturedProjects = () => dispatch => {
         'Google AdMob'
       ],
       imageURL:
-        'https://docs.google.com/drawings/d/1NZenN700uCF4OEvn0BstWG4r-dlZVNFkP91xjyLaPe4/pub?w=1049&h=1049'
+        'https://docs.google.com/drawings/d/1NZenN700uCF4OEvn0BstWG4r-dlZVNFkP91xjyLaPe4/pub?w=1049&h=1049',
+      projectURL: 'https://krlargo.github.io/QuickSplit'
     }
   ];
 
